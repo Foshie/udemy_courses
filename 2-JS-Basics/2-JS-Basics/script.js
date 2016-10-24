9aca3a3597fad814c3d2e65ae0270918e25ad3ec
Joshua Fann <Joshua Fann>
2016-10-23 17:07:41 -0700

    10.23.2016 first set of lectures

---
+++ 2-JS-Basics/2-JS-Basics/script.js
@@ -0,0 +1,142 @@
+// Lecture: variables
+/*
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+console.log(name + ' ' + lastName + ' is ' + age + ' year\'s old');
+
+var fullAge = true;
+
+console.log(fullAge);
+*/
+
+// Lecture: variables 2
+/*var name = 'John';
+var age = 26;
+
+// console.log(name + age);
+
+var job, isMarried;
+
+// console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old ' + job + 
+	'. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+console.log(name + ' is a ' + age + ' year old ' + job + 
+	'. Is he married? ' + isMarried + '.');
+
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
+alert(name + ' is a ' + age + ' year old ' + job + 
+	'. Is he married? ' + isMarried + '.')
+*/
+
+// // Lecture: operators
+// var now = 2016;
+// var birthYear = now - 26;
+
+// birthYear = now - 26*2;
+
+// console.log(birthYear);
+
+// var ageJohn = 30;
+// var ageMark = 30;
+
+// ageJohn = 3 + 5 *4 - 6;
+
+// Lecture: if/else statements
+/*
+var name = 'John';
+var age = 26;
+var isMarried = 'no';
+
+if(isMarried === 'yes'){
+	console.log(name + ' is married!');
+} else{
+	console.log(name + ' will hopefully marry soon');
+}
+
+isMarried = false;
+
+if(isMarried){
+	console.log('YES');
+} else{
+	console.log('NO');
+}
+*/
+
+// Lecture: boolean logic and switch
+
+// var age = 16;
+
+// if(age < 20){
+// 	console.log('John is a teenager');
+// } else {
+// 	console.log('John is a man');
+// }
+
+// var job = 'teacher';
+// job = prompt('What does john do?')
+
+// switch(job){
+// 	case 'teacher':
+// 		console.log('John teaches kids.');
+// 		break;
+// 	case 'driver':
+// 		console.log('John drives a cab in Lisbon.');
+// 		break;
+// 	case 'cop':
+// 		console.log('John helps fight crime');
+// 		break;
+// 	default:
+// 		console.log('John does something else');
+
+// }
+
+//Coding Challenge 1
+/*
+var johnsHeight;
+var friendsHeight;
+var johnsAge;
+var friendsAge;
+
+var johnScore = johnsHeight + 5 * johnsAge;
+var friendsScore = friendsHeight + 5 * friendsAge;
+
+if(johnScore > friendsScore){
+	console.log('John wins');
+}else if(johnScore < friendsScore){
+	console.log('Friend A wins');
+}else{
+	console.log('nobody wins');
+}
+*/
+
+//Lecture: functions
+
+function calculateAge(yearOfBirth){
+	var age = 2016 - yearOfBirth;
+	return age;
+
+}
+
+var ageJohn = calculateAge(1990);
+console.log(ageJohn);
+
+function yearsUntilRetirement(name, yearOfBirth){
+
+	var age = calculateAge(yearOfBirth);
+	
+}
